e501951578b14c38c301a2be88156d93450f28de
Marco Alexi Sta Ana <alexistaana@yahoo.com.ph>
2026-08-13 04:13:30 -0500

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        s_dict = dict()
+        t_dict = dict()
+        for i,j in zip(s,t):
+            s_dict[i] = s_dict.get(i, 0) + 1
+            t_dict[j] = t_dict.get(j, 0) + 1
+
+        return True if s_dict == t_dict else False
+        
